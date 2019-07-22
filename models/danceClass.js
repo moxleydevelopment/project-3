88790329122f9f5ed7a9c4a6724860309fc81f11
Donya Moxley <moxleydevelopment@gmail.com>
2019-07-22 09:46:29 -0400

    added functions to class model

---
+++ models/danceClass.js
@@ -0,0 +1,48 @@
+const mongoose = require('./connection.js')
+
+
+const DanceClassSchema = new mongoose.Schema({
+ name: String,
+ instructor: String,
+ classSize: Number,
+ studioId: String
+})
+
+const DanceClassCollection = mongoose.model('DanceClass', DanceClassSchema)
+
+
+
+function getAllDanceCLass() {
+  return DanceClassCollection.find()
+}
+
+function getDanceClass(classId){
+    return DanceClassCollection.findById(classId)
+}
+
+function getClassByStudioId(studioId){
+    return DanceClassCollection.find(
+        {"studioId" : studioId}
+    )
+}
+
+function addNewDanceClass(danceClass){
+    return DanceClassCollection.create(danceClass)
+}
+
+function updateDanceClass(classId, danceClass){
+    return DanceClassCollection.findByIdAndUpdate(classId, danceClass, {new: true})
+
+}
+
+function deleteDanceClass(classId){
+    return DanceClassCollection.findByIdAndUpdate(classId)
+}
+module.exports = {
+  getAllDanceCLass,
+  getDanceClass,
+  getClassByStudioId,
+  addNewDanceClass,
+  updateDanceClass,
+  deleteDanceClass
+}
