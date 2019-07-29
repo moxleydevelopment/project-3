cfddce7afca93afb0f85f82229d5f4f984a6cce5
Donya Moxley <moxleydevelopment@gmail.com>
2019-07-29 12:08:56 -0400

    deleted all template code

--- server.js
+++ server.js
@@ -22,27 +22,15 @@ app.use(express.static(`${__dirname}/client/build`))
 app.use('/api/dancestudio', DanceStudioRouter)
 app.use('/api/dancestudio/:studioId/danceclass', DanceClassRouter)
 
-/* Step 5
- *
- * add catch all route to serve up the built react app for any request not made to our
- * /api/... routes.
- */
+
 app.get('/*', (req, res) => {
     res.sendFile(`${__dirname}/client/build/index.html`)
 })
 
-/* Step 6
- *
- * Set the port the server is to run on
- *
- * NOTE: keep these lines at the bottom of the file 
- */
+
 const PORT = process.env.PORT || 3001
 
-/* Step 7
- *
- * Start the server
- */
+
 app.listen(PORT, () => {
     console.log(`App is listening on PORT ${PORT}`)
 })
